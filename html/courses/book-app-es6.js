1a27f3adb1d2536fe61e34d44d87e54b9f8069a3
micah.wehrle <micah23@gmail.com>
2022-08-07 18:06:44 -0400

    Added book list and orbit physics

---
+++ html/courses/book-app-es6.js
@@ -0,0 +1,177 @@
+/*
+    Now that I survived the ES5 class script, I will be designing this script without watching Brad's video.
+    I will also attempt to add the local storage feature, and spice it up as I see fit.
+
+    Ok, I think that about does it. I was able to do most of it from memory, and only copied some of the code that needed to be done a very specific way (such as the UI static methods)
+    I also added some smarter filtering, so you can't add two books with the same ISBN. I also added an error notice if for some reason a book fails to load (which in this instance may only be duplicate ISBN)
+    To be honest (I am only guessing this is how Brad will do it, and maybe this falls under his "I'm doing things simple now, we will learn better practice later" clause), but I don't like my operational method of storing the books as being just in the html
+        I would rather have an array of books and then update that when I add/remove books, and use it when I save/load the books from storage.
+        I won't implement this here, but I hope to be able to in a future project.
+    
+    Since it's all working how I want, I will watch his conversion to ES6 video as well as his storage feature video
+    Here are my thoughts and changes:
+        
+*/
+
+// Book Class
+class Book {
+
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class Storage {
+    
+    constructor() { // Not sure if this really matters. Wish I could make static classes
+        console.error('Storage is not meant to be instantiated!');
+    }
+
+    static loadBooks() { // Pulls book list from local storage and creates all 
+        const bookStorage = localStorage.getItem('books');
+
+        if(bookStorage !== null && bookStorage !== '[]') {
+            let failedBooks = 0;
+            let books = JSON.parse(bookStorage);
+            books.forEach(function(book) {
+                // Counts how many timess addBook returns false
+                failedBooks += !UI.addBook(new Book(book[0], book[1], book[2])); // See this new Book crap is silly on this scale. 
+            });
+
+            if(failedBooks === 0) { // Let's the user know how many books were loaded
+                UI.showAlert(`Sucessfully loaded ${books.length} book(s).`, 'success'); // I could add a ternary here as I have done in previous projects but considering the fact I made this project twice I'm just ready to move on
+            }
+            else {
+                UI.showAlert(`Loaded ${books.lenth - failedBooks} sucessfully but failed to load ${failedBooks} book(s)`, 'error');
+            }
+        }
+    }
+
+    // Go through the HTML and yank out every book, and add it to storage.
+
+    static saveBooks() {
+        const list = document.getElementById('book-list');
+        let books = [];
+        list.childNodes.forEach(function(bookData) {
+            let book = [];
+            for(let i = 0; i < 3; i++) {
+                book.push(bookData.children[i].innerHTML);
+            }
+            books.push(book);
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+}
+
+// Wish I could make a static class
+class UI {
+    constructor() {
+        console.error('UI is not meant to be instantiated!');
+    }
+
+    // The following 3  pulled form ES5 script
+    static addBook(book) {
+        const list = document.getElementById('book-list');
+        
+        // Not sure the best way to do this. This will hopefully be the most time and space efficient
+        let foundDuplicate = false;
+        list.childNodes.forEach(function(existingBook) {
+            
+            if(foundDuplicate) {
+                return;
+            }
+
+            if(book.isbn === existingBook.children[2].innerHTML) {
+                foundDuplicate = true;
+                return;
+            }
+        });
+
+        if(foundDuplicate) {
+            return false;
+        }
+
+
+
+        // Create tr (table row)
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete">[X]<a></td>
+        `;
+
+        list.appendChild(row);
+
+        return true;
+    }
+
+    static clearFields() {
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+    }
+
+    static showAlert(msg, className) {
+        // Create div
+        const div = document.createElement('div');
+        // Add classess
+        div.className = 'alert ' + className;
+    
+        div.appendChild(document.createTextNode(msg));
+    
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+    
+        container.insertBefore(div, form);
+    
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+}
+
+// Grab necessary UI elements
+const titleForm = document.getElementById('title'),
+    authorForm = document.getElementById('author'),
+    isbnForm = document.getElementById('isbn');
+
+// Listeners
+document.getElementById('book-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    if(titleForm.value !== '' && authorForm.value !== '' && isbnForm.value !== '') {
+        // I understand this is a hypothetical framework for an expandable application but this seems silly. Book class seems pointless.
+        const book = new Book(titleForm.value, authorForm.value, isbnForm.value);
+        
+        if(UI.addBook(book)) {
+            Storage.saveBooks();
+            UI.showAlert('Book added!', 'success');
+            UI.clearFields();
+        }
+        else {
+            UI.showAlert('That book is already in the list!', 'error');
+        }
+    }
+    else {
+        UI.showAlert('Please make sure all fields are filled.', 'error');
+    }
+    
+});
+
+document.getElementById('book-list').addEventListener('mousedown', function(e) {
+
+    if(e.target.className === 'delete') {
+        e.target.parentElement.parentElement.remove();
+        Storage.saveBooks();
+        UI.showAlert('Book removed successfully.', 'success');
+    }
+
+    e.preventDefault();
+});
+
+Storage.loadBooks();
---
+++ html/courses/orbit-app.js
@@ -0,0 +1,304 @@
+/*
+    This is just a fun project to play around with ES6 classes
+
+    Hey listen. I had to do some jank stuff to get this to work. 
+        Specifically I am struggling to understand what happens when I do a forEach loop on an array within a class.
+            What is "this"? I thought it would be object that is having the method called from it, but I guess maybe it's the loop? or the function?
+        I probably in fixing this did some stuff that is illegal. I am still learning. Also none of my distance functions work except the static one. Not sure what that's about, probably more "this" confusion. I mean I understand "this" calls the object you're working in, but why is "this" not working sometimes. I'm losing the context I guess, but I don't know how to maintain it other than just doing like const curThis = this. stupid.
+
+*/
+
+// Get canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const w = canvas.width;
+const h = canvas.height;
+
+const bodyColorPicker = document.getElementById('body-color-picker');
+
+
+// Some detail variables
+const borderSize = 2;
+const colors = ['red', 'orange', 'gray', 'green', 'blue', 'purple'];
+const maxNewCircleSize = 50;
+const massMult = 1; // Need to try a size (mass) multiplier instead
+const shootVelMult = 0.05;
+const gravity = 0.22;
+const ignoreDistance = 500;
+const deleteDistance = 1000;
+const closestPossible = 0;
+
+
+
+// Some control settings
+let inputMode = ['wait', ''];
+let bodies = [];
+let tempBody = null;
+let uniqueID = 0;
+let toggles = {
+    'physics': false,
+    'showVel': false,
+    'randColor': true,
+    'isStatic': false,
+};
+
+
+
+
+
+//  Body class
+class Body {
+
+    constructor(x, y, size, velX, velY, color, isStatic) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.mass = size*massMult;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = typeof color === 'undefined' || color === '' 
+            ? (toggles.randColor ? colors[Math.floor((Math.random()*colors.length))] : bodyColorPicker.value) 
+            : color;
+        this.isStatic = typeof isStatic === 'undefined' ? false : isStatic;
+
+        this.xx = -1;
+        this.yy = -1;
+
+        this.uid = uniqueID++;
+    }
+
+
+
+    static distance(x1, y1, x2, y2) {
+        return Math.sqrt( Math.pow(x2-x1,2) + Math.pow(y2-y1,2) );
+    }
+
+    distance(body) {
+        return Body.distance(this.x, this.y, body.x, body.y);
+    }
+
+    distance(x, y) {
+        return Body.distance(this.x, this.y, x, y);
+    }
+
+    grav() {
+        const thisBody = this;
+
+        
+        bodies.forEach(function(body) {
+            
+            if(thisBody.uid !== body.uid && Body.distance(thisBody.x, thisBody.y, body.x, body.y) < ignoreDistance) {
+
+                const xDist = thisBody.x - body.x;
+                const yDist = thisBody.y - body.y;
+
+                const totalDist = Body.distance(thisBody.x, thisBody.y, body.x, body.y);
+
+                const force = gravity * ((thisBody.mass*body.mass) / (totalDist*totalDist));
+
+
+    
+                thisBody.velX += force * (-xDist/totalDist);
+                thisBody.velY += force * (-yDist/totalDist);
+            }
+            
+        });
+    }
+
+    move() {
+        if(!this.isStatic) {
+
+            const newX = this.x + this.velX;
+            const newY = this.y + this.velY;
+
+            for(let i = 0; i < bodies.length; i++) {
+                if(bodies[i] === this) {
+                    continue;
+                }
+
+                if(Body.distance(newX, newY, bodies[i].x, bodies[i].y) < this.size + bodies[i].size) {
+                    this.velX = -this.velX;
+                    this.velY = -this.velY;
+                }
+            }
+
+            
+
+            this.x += this.velX;
+            this.y += this.velY;
+        }
+    }
+
+    render() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, 360);
+        ctx.fill();
+
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, 360);
+        ctx.stroke();
+
+        if(this.isStatic) {
+            const lineSize = this.size/4;
+            ctx.lineWidth = 1;
+            ctx.beginPath();
+            ctx.moveTo(this.x-lineSize, this.y-lineSize);
+            ctx.lineTo(this.x+lineSize, this.y+lineSize);
+
+            ctx.moveTo(this.x-lineSize, this.y+lineSize);
+            ctx.lineTo(this.x+lineSize, this.y-lineSize);
+            ctx.stroke();
+        }
+    }
+}
+
+function toggleBtn(element) {
+    element.classList.toggle('enabled');
+    toggles[element.id] = !toggles[element.id];
+}
+
+function triggerBtn(element) {
+    switch(element.id) {
+        case "clear":
+            bodies = [];
+            break;
+        
+        case "default":
+            defaultBalls();
+            break;
+    }
+}
+
+
+
+canvas.addEventListener('mousedown', function(e) {
+    const rect = e.target.getBoundingClientRect(); // found this online
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    
+    switch(inputMode[0]) {
+        case 'wait':
+            //console.log(bodies[0].velX);
+            inputMode = ['draw','newCircle'];
+
+            tempBody = new Body(x, y, 1);
+            tempBody.isStatic = toggles.isStatic;
+            break;
+        case 'draw':
+            if(inputMode[1] === 'newCircle') {
+                if(tempBody.isStatic) {
+                    bodies.push(new Body(tempBody.x, tempBody.y, tempBody.size, 0, 0, tempBody.color, true));
+                    inputMode = ['wait', ''];
+                    tempBody = null;
+                }
+                inputMode[1] = 'strength';
+                tempBody.xx = x;
+                tempBody.yy = y;
+            }
+            else if(inputMode[1] === 'strength') {
+                // launch!
+                bodies.push(new Body(tempBody.x, tempBody.y, tempBody.size, 
+                    (tempBody.x - tempBody.xx)*shootVelMult,
+                    (tempBody.y - tempBody.yy)*shootVelMult,
+                    tempBody.color, false));
+                inputMode = ['wait', ''];
+                tempBody = null;
+            }
+        default:
+            break;
+    }
+});
+
+canvas.addEventListener('mousemove', function(e) {
+    const rect = e.target.getBoundingClientRect(); // found this online
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    switch(inputMode[0]) {
+        case 'draw':
+            if(inputMode[1] === 'newCircle') {
+                tempBody.size = Math.min(Math.round(tempBody.distance(x,y)/2), maxNewCircleSize);
+            }
+            else if(inputMode[1] === 'strength') {
+                tempBody.xx = x;
+                tempBody.yy = y;
+            }
+            break;
+        default:
+            break;
+    }
+
+});
+
+function draw() {
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.clearRect(borderSize, borderSize, canvas.width - borderSize*2, canvas.height - borderSize*2);
+
+    if(tempBody !== null) {
+        tempBody.render();
+    }
+    
+    if(inputMode[0] === 'draw' && inputMode[1] === 'strength') {
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(tempBody.x, tempBody.y);
+        ctx.lineTo(tempBody.x + (tempBody.x-tempBody.xx), tempBody.y + (tempBody.y-tempBody.yy));
+        ctx.stroke();
+    }
+
+    if(toggles.physics) {
+        bodies.forEach(function(body) {body.grav();});
+        bodies.forEach(function(body) { body.move(); });
+    }
+
+    for(let i = bodies.length-1; i >= 0; i--) { // chop out stuff that gets too far away
+        if(Body.distance(canvas.width/2, canvas.height/2, bodies[i].x, bodies[i].y) > deleteDistance) {
+            bodies.splice(i, 1);
+        }
+    }
+
+    bodies.forEach(function(body) {body.render();});
+    
+    if(toggles.showVel) {
+        bodies.forEach(function(body) {
+            if(!body.isStatic) {
+                ctx.strokeStyle = 'black';
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(body.x, body.y);
+                ctx.lineTo(body.x + body.velX/shootVelMult, body.y + body.velY/shootVelMult);
+                ctx.stroke();
+            }
+        });
+    }
+    
+
+
+    
+    window.requestAnimationFrame(draw);
+}
+
+//bodies.push(new Body(100,100,30,0,0));
+//bodies.push(new Body(150,150,30,0,0));
+
+function defaultBalls() {
+    bodies = [];
+
+    bodies.push(new Body(250, 250, 50, 0, 0, 'green', true));
+
+    bodies.push(new Body(400, 250, 10, 0, 0.9, 'orange'));
+    bodies.push(new Body(100, 250, 20, 0, -1.2, 'red'));
+}
+defaultBalls();
+
+// bodies.push(new Body(380, 250, 1, -0.1, 1.2, 'black'));
+
+draw();
